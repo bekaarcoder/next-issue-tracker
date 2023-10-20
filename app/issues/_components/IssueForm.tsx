fa9166f62f86f820fa01b66c16370cc5a2ff3835
Shashank <s_shashank@outlook.com>
2023-10-21 00:36:06 +0530

    Updating Issue

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -41,7 +41,11 @@ const IssueForm = ({ issue }: Props) => {
     const onSubmit = handleSubmit(async (data) => {
         try {
             setIsSubmitting(true);
-            await axios.post('/api/issues', data);
+            if (issue) {
+                await axios.patch(`/api/issues/${issue.id}`, data);
+            } else {
+                await axios.post('/api/issues', data);
+            }
             router.push('/issues');
         } catch (error) {
             setIsSubmitting(false);
