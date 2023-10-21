c1b19f574d9273164d3dbb9aa63a19c379530e01
Shashank <s_shashank@outlook.com>
2023-10-21 12:39:05 +0530

    added API to delete issue

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -18,7 +18,7 @@ export async function PATCH(
     });
 
     if (!issue) {
-        return NextResponse.json({ error: 'Issue not found' }, { status: 400 });
+        return NextResponse.json({ error: 'Issue not found' }, { status: 404 });
     }
 
     const updatedIssue = await prisma.issue.update({
@@ -30,4 +30,23 @@ export async function PATCH(
     });
 
     return NextResponse.json(updatedIssue);
+}
+
+export async function DELETE(
+    request: NextRequest,
+    { params }: { params: { id: string } }
+) {
+    const issue = await prisma.issue.findUnique({
+        where: { id: parseInt(params.id) },
+    });
+
+    if (!issue) {
+        return NextResponse.json({ error: 'Issue not found' }, { status: 404 });
+    }
+
+    await prisma.issue.delete({
+        where: { id: issue.id },
+    });
+
+    return NextResponse.json({});
 }
