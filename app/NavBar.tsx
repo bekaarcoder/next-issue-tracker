a907c03f7db7b52a4196daad39209ae18c2e86a5
Shashank <s_shashank@outlook.com>
2023-10-22 12:22:51 +0530

    fixed avatar not loading

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -6,7 +6,14 @@ import React from 'react';
 import { FaBug } from 'react-icons/fa';
 import classnames from 'classnames';
 import { useSession } from 'next-auth/react';
-import { Box, Container, Flex } from '@radix-ui/themes';
+import {
+    Avatar,
+    Box,
+    Container,
+    DropdownMenu,
+    Flex,
+    Text,
+} from '@radix-ui/themes';
 
 const NavBar = () => {
     const currentPath = usePathname();
@@ -53,7 +60,27 @@ const NavBar = () => {
                     </Flex>
                     <Box>
                         {status === 'authenticated' && (
-                            <Link href="/api/auth/signout">Logout</Link>
+                            <DropdownMenu.Root>
+                                <DropdownMenu.Trigger>
+                                    <Avatar
+                                        src={session.user!.image!}
+                                        fallback="?"
+                                        radius="full"
+                                        size={'2'}
+                                        className="cursor-pointer"
+                                    />
+                                </DropdownMenu.Trigger>
+                                <DropdownMenu.Content>
+                                    <DropdownMenu.Label>
+                                        <Text>{session.user!.name}</Text>
+                                    </DropdownMenu.Label>
+                                    <DropdownMenu.Item>
+                                        <Link href="/api/auth/signout">
+                                            Logout
+                                        </Link>
+                                    </DropdownMenu.Item>
+                                </DropdownMenu.Content>
+                            </DropdownMenu.Root>
                         )}
                         {status === 'unauthenticated' && (
                             <Link href="/api/auth/signin">Login</Link>
