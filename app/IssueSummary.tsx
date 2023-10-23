0c263ae08691b5df88eaea044b11e8523da2095e
Shashank <s_shashank@outlook.com>
2023-10-23 13:59:43 +0530

    added issue summary component

---
+++ app/IssueSummary.tsx
@@ -0,0 +1,43 @@
+import { Status } from '@prisma/client';
+import { Card, Flex, Text } from '@radix-ui/themes';
+import Link from 'next/link';
+import React from 'react';
+
+interface Props {
+    open: number;
+    inProgress: number;
+    closed: number;
+}
+
+const IssueSummary = ({ open, inProgress, closed }: Props) => {
+    const statuses: { label: string; value: number; status: Status }[] = [
+        { label: 'Open', value: open, status: 'OPEN' },
+        {
+            label: 'In Progress',
+            value: inProgress,
+            status: 'IN_PROGRESS',
+        },
+        { label: 'Closed', value: closed, status: 'CLOSED' },
+    ];
+    return (
+        <Flex gap={'4'}>
+            {statuses.map((status) => (
+                <Card key={status.label}>
+                    <Flex direction={'column'} gap={'2'}>
+                        <Link
+                            href={`/issues?status=${status.status}`}
+                            className="text-sm font-medium"
+                        >
+                            {status.label}
+                        </Link>
+                        <Text size={'5'} className="font-bold">
+                            {status.value}
+                        </Text>
+                    </Flex>
+                </Card>
+            ))}
+        </Flex>
+    );
+};
+
+export default IssueSummary;
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,22 @@
 import Image from 'next/image';
 import Pagination from './components/Pagination';
 import LatestIssues from './LatestIssues';
+import IssueSummary from './IssueSummary';
+import prisma from '@/prisma/client';
 
-export default function Home() {
-    return <LatestIssues />;
+export default async function Home() {
+    const openIssues = await prisma.issue.count({ where: { status: 'OPEN' } });
+    const inProgressIssues = await prisma.issue.count({
+        where: { status: 'IN_PROGRESS' },
+    });
+    const closedIssues = await prisma.issue.count({
+        where: { status: 'CLOSED' },
+    });
+    return (
+        <IssueSummary
+            open={openIssues}
+            inProgress={inProgressIssues}
+            closed={closedIssues}
+        />
+    );
 }
